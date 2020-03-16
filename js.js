654ae63edcca7b725a856dacbf58ecc684621680
brilook <brilook@gmail.com>
2020-03-16 19:39:20 +0200

    add css + changes js

--- js.js
+++ js.js
@@ -1,1 +1,16 @@
 'use strict';
+let money = +prompt('Ваш бюджет на месяц?', '100');
+let time = prompt('Введите дату в формате YYYY-MM-DD');
+let bindingExpenses = prompt('Введите обязательную статью расходов в этом месяце', 'Туалбум');
+let bindingExpensesCost = +prompt('Во сколько обойдется?');
+let appData = {
+    budget: money,
+    timeData: time,
+    expenses: {
+    bindingExpenses: bindingExpensesCost,
+    },
+    income: [],//необязательные расходы
+    savings: false,
+};
+
+
