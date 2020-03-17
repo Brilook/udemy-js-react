80db6f8b6f34f92d39a8f60a556f746cbb0a7687
brilook <brilook@gmail.com>
2020-03-17 09:51:14 +0200

    dailyBudget

--- js.js
+++ js.js
@@ -1,16 +1,29 @@
 'use strict';
 let money = +prompt('Ваш бюджет на месяц?', '100');
 let time = prompt('Введите дату в формате YYYY-MM-DD');
-let bindingExpenses = prompt('Введите обязательную статью расходов в этом месяце', 'Туалбум');
-let bindingExpensesCost = +prompt('Во сколько обойдется?');
+let requiredExpenses = prompt('Введите обязательную статью расходов в этом месяце', 'Туалбум');
+let requiredExpensesCost = +prompt('Во сколько обойдется?');
+let requiredExpenses2 = prompt('Введите обязательную статью расходов в этом месяце', 'Туалбум');
+let requiredExpensesCost2 = +prompt('Во сколько обойдется?');
 let appData = {
     budget: money,
     timeData: time,
     expenses: {
-    bindingExpenses: bindingExpensesCost,
+    requiredExpenses: requiredExpensesCost,
+    requiredExpenses2: requiredExpensesCost2,
     },
-    income: [],//необязательные расходы
+    optionalExpenses: {//необязательные расходы
+
+    },
+    income: [],//доп. доход
     savings: false,
 };
+
+let month = 30;
+
+let dailyBudget = money / month;
+
+alert (`бюбжет на день ${dailyBudget}`);
+
 
 
